cf128b46d20929425d2636e5a394492fe8e2e00e
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-11-04 16:16:31 +0800

    feat: support mmu_alterable
    - impl enable and disable submod for mmu_alterable
    - learn how to use conditional compilation
    
    authored by: Miao Hao

---
+++ lesson1/mmu_alterable/src/disable/mod.rs
@@ -0,0 +1,8 @@
+pub unsafe fn pre_mmu() {
+}
+
+pub unsafe fn enable_mmu() {
+}
+
+pub unsafe fn post_mmu() {
+}
--- lesson1/mmu_alterable/src/lib.rs
+++ lesson1/mmu_alterable/src/lib.rs
@@ -1,15 +1,24 @@
 #![no_std]
+#![feature(asm_const)]
+
+mod enable;
+mod disable;
+
+#[cfg(feature = "enable")]
+use enable as mmu;
+#[cfg(feature = "disable")]
+use disable as mmu;
 
 pub const KERNEL_BASE: usize = 0xffff_ffff_c000_0000;
 
 pub unsafe fn pre_mmu() {
-    todo!("dummy");
+    mmu::pre_mmu();
 }
 
 pub unsafe fn enable_mmu() {
-    todo!("dummy");
+    mmu::enable_mmu();
 }
 
 pub unsafe fn post_mmu() {
-    todo!("dummy");
+    mmu::post_mmu();
 }
